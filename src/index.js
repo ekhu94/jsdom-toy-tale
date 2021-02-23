70cdeb66e750edbfe71c5e2b3bc26aae117f2649
Erik Huang <erikhuangdrums@gmail.com>
2021-02-23 10:48:26 -0800

    commit tree 9a5fb8e3ee945ba7878f426a5f57616cc1bf1ceb

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
+const toyCollection = document.querySelector('#toy-collection');
+const form = document.querySelector('#add-toy-form');
 let addToy = false;
+
+const makeToys = toys => {
+
+  for (let toy of toys) {
+    let div = document.createElement('div')
+    let h2 = document.createElement('h2')
+    let img = document.createElement('img')
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+
+    h2.innerText = toy.name
+    img.src = toy.image
+    img.style.width = "200px";
+    p.innerText = `${toy.likes} Likes`
+    btn.classList.add('like-btn')
+    btn.innerText = 'Like <3'
+
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(p)
+    div.appendChild(btn)
+
+    toyCollection.appendChild(div)
+    div.classList.add('card')
+  }
+
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(json => makeToys(json))
+})
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
