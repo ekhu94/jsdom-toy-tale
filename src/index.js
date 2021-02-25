9cced733f4ec7b2e2a6a760d4c68b02a12e07a97
Erik Huang <erikhuangdrums@gmail.com>
2021-02-24 21:20:36 -0800

    get functions done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  getToys()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +14,30 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
 });
+
+const getToys = () => {
+  axios.get('http://localhost:3000/toys').then(resp => {
+    for (let toy of resp.data) createToy(toy)
+  })
+}
+
+const createToy = toy => {
+  const collection = document.querySelector('#toy-collection')
+  const div = document.createElement('div')
+  const h2 = document.createElement('h2')
+  const img = document.createElement('img')
+  const p = document.createElement('p')
+  const btn = document.createElement('button')
+
+  div.classList.add('card')
+  h2.innerText = toy.name
+  img.classList.add('toy-avatar')
+  img.src = toy.image
+  p.innerText = `${toy.likes} Likes`
+  btn.classList.add('like-btn')
+
+  div.append(h2, img, p, btn)
+  collection.appendChild(div)
+}
